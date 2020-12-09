fb6455cc1b4bb892efa09d3cbc447c36e4b0f7a6
chrapekuba@gmail.com <chrapekuba@gmail.com>
2020-12-09 07:10:06 +0100

    CoursesStagesSection fonts update

--- src/components/SectionsComponents/CoursesStagesSection.js
+++ src/components/SectionsComponents/CoursesStagesSection.js
@@ -16,12 +16,36 @@ const StyledContentWrapper = styled(ContentWrapper)`
   @media only screen and (max-width: 889px) {
     padding: 80px 30px 90px;
   }
+  @media only screen and (max-width: 798px) {
+    padding: 40px 30px 190px;
+    flex-direction: column;
+  }
 `
 
 const StyledParagraph = styled(Paragraph)`
   @media only screen and (max-width: 1081px) {
     font-size: 30px;
     margin: 0 32px 0 5px;
+  }
+  @media only screen and (max-width: 798px) {
+    margin-top: 26px;
+  }
+`
+
+const StyledCapitalizedText = styled(CapitalizeText)`
+  @media only screen and (max-width: 645px) {
+    font-size: 10px;
+    line-height: 12px;
+    letter-spacing: 4px;
+  }
+`
+
+const StyledColumnFlex = styled(Flex)`
+  @media only screen and (max-width: 798px) {
+    margin-top: 50px;
+  }
+  @media only screen and (max-width: 645px) {
+    margin-top: 24px;
   }
 `
 
@@ -30,17 +54,22 @@ const CoursesStagesSection = () => {
     <Wrapper padding="0" bg="light">
       <StyledContentWrapper padding="90px 107px 110px 102px">
         <Flex margin="0 16px 0 0" flex="1" flexDirection="column">
-          <CapitalizeText margin="0 0 0 5px">
+          <StyledCapitalizedText margin="0 0 0 5px">
             Etapy kursu od zera
-          </CapitalizeText>
+          </StyledCapitalizedText>
           <Line />
           <StyledParagraph margin="0 52px 0 5px">
             Tempo, w jakim chcesz robić postępy, dostosujesz do własnych,
             indywidualnych potrzeb i&nbsp;możliwości.
           </StyledParagraph>
         </Flex>
-        <Flex margin="140px 0 0" flex="1" flexDirection="column">
-          <Paragraph fontSize="18px" lineHeight="1.44em" letterSpacing="1px">
+        <StyledColumnFlex margin="140px 0 0" flex="1" flexDirection="column">
+          <Paragraph
+            fontSize="18px"
+            lineHeight="1.44em"
+            letterSpacing="1px"
+            fontFamily="Lato"
+          >
             Jeżeli dopiero zaczynasz - zacznij od WPROWADZENIA. Jest to część
             wstępna kursu, dzięki której zadomowisz się w podstawach języka
             włoskiego i dalsza samodzielna nauka będzie znacnzie łatwiejsza.
@@ -50,6 +79,7 @@ const CoursesStagesSection = () => {
             fontSize="18px"
             lineHeight="1.44em"
             letterSpacing="1px"
+            fontFamily="Lato"
           >
             Kolejny etap, czyli CZĘŚĆ PIERWSZA, pomoże Ci zacząć mówić o sobie
             i&nbsp;używać włoskich czasowników.
@@ -59,6 +89,7 @@ const CoursesStagesSection = () => {
             fontSize="18px"
             lineHeight="1.44em"
             letterSpacing="1px"
+            fontFamily="Lato"
           >
             Obie te części kursu przygotują Cię do dalszej nauki, którą możesz
             kontunuować samodzielnie, korzystając z dostępnych odpłatnie
@@ -66,7 +97,7 @@ const CoursesStagesSection = () => {
             częściach “Kursu języka włoskiego od zera” pojawią się w zakłądce
             KONTYNUACJA.
           </Paragraph>
-        </Flex>
+        </StyledColumnFlex>
       </StyledContentWrapper>
     </Wrapper>
   )
