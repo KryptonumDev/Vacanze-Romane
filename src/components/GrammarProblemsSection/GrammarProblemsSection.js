405418333f215fc5a62cc7e2387af4b2015910e9
jakubchrapek <chrapekuba@gmail.com>
2021-12-15 19:03:42 +0100

    Italian grammar problems mobile version

--- src/components/GrammarProblemsSection/GrammarProblemsSection.js
+++ src/components/GrammarProblemsSection/GrammarProblemsSection.js
@@ -15,21 +15,36 @@ const ContentWrapper = styled.div`
   width: 100%;
   max-width: 1440px;
   margin: clamp(80px, 7.27vw, 103px) auto clamp(300px, 23.88vw, 344px) auto;
-  padding: clamp(90px, 8.47vw, 122px) clamp(60px, 5.55vw, 80px)
-    clamp(272px, 20.69vw, 298px) clamp(82px, 7.29vw, 105px);
+  padding: clamp(90px, 8.47vw, 122px) 82px clamp(272px, 20.69vw, 298px)
+    clamp(82px, 7.29vw, 105px);
   position: relative;
   display: flex;
   flex-direction: column;
+
+  @media (max-width: 798px) {
+    margin-top: 0;
+    margin-bottom: 0;
+    padding: 72px 30px;
+  }
 `
 
 const SectionTitle = styled(CapitalizeText)`
   color: var(--beige-2);
+  @media only screen and (max-width: 798px) {
+    font-size: 10px;
+    margin-left: 10px;
+    letter-spacing: 4px;
+    line-height: 1.5;
+  }
 `
 
 const SectionQuestion = styled(Paragraph)`
   color: var(--beige-2);
   @media (max-width: 1181px) {
     font-size: 28px;
+  }
+  @media (max-width: 798px) {
+    font-size: 18px;
   }
 `
 
@@ -43,6 +58,7 @@ const SectionQuestionItalian = styled.p`
   }
   @media only screen and (max-width: 798px) {
     font-size: 18px;
+    margin-top: 24px;
   }
   line-height: 1.11;
   letter-spacing: 1px;
@@ -62,6 +78,9 @@ const SectionParagraph = styled.p`
       font-size: 28px;
       max-width: 820px;
     }
+    @media only screen and (max-width: 798px) {
+      font-size: 18px;
+    }
   }
 `
 
@@ -69,6 +88,7 @@ const LinkToArticle = styled(Link)`
   font-size: 24px;
   line-height: 1.175;
   letter-spacing: 1px;
+  text-decoration: none;
   color: var(--black);
   font-weight: 400;
   margin-top: 52px;
@@ -77,6 +97,12 @@ const LinkToArticle = styled(Link)`
   border: none;
   width: fit-content;
   cursor: pointer;
+
+  @media (max-width: 798px) {
+    font-size: 18px;
+    padding: 16px 23px;
+    margin-top: 44px;
+  }
 
   transition: background-color 0.3s cubic-bezier(0.39, 0.575, 0.565, 1),
     color 0.3s cubic-bezier(0.39, 0.575, 0.565, 1);
@@ -121,6 +147,28 @@ const ImagesWrapper = styled.div`
       }
     }
   }
+  @media (max-width: 798px) {
+    position: relative;
+    right: unset;
+    bottom: unset;
+    flex-direction: column;
+    justify-content: flex-start;
+    margin-top: 50px;
+
+    .gatsby-image-wrapper {
+      width: 100%;
+      max-width: 100%;
+      margin: 0;
+
+    &:nth-of-type(2) {
+      margin: 27px 0 0 0;
+    }
+
+    picture,
+  img {
+    aspect-ratio: 321/375;
+  }
+  }
 `
 
 const GrammarProblemsSection = ({
@@ -144,7 +192,9 @@ const GrammarProblemsSection = ({
         <SectionParagraph>
           <StructuredText data={paragraphText.value} />
         </SectionParagraph>
-        <LinkToArticle>{buttonText}</LinkToArticle>
+        <LinkToArticle to="/in-italiano/gramatyka-wloska">
+          {buttonText}
+        </LinkToArticle>
         <ImagesWrapper>
           <GatsbyImage image={fluidImg1} />
           <GatsbyImage image={fluidImg2} />
